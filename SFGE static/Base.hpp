8dc7707386ad7cc6df756a4382838e2e00900775
Karol1165 <159713913+Karol1165@users.noreply.github.com>
2024-07-15 17:18:45 +0200

    cd

--- SFGE static/Base.hpp
+++ SFGE static/Base.hpp
@@ -56,7 +56,7 @@ namespace SFGF {
 
 	class UI : public sf::Drawable {
 	public:
-		virtual void CheckStatus(const sf::Event& e, const sf::Time& deltaTime = sf::Time(sf::seconds(0)), const sf::Vector2f& mousePos = sf::Vector2f(0, 0)) = 0;
+		virtual void CheckStatus(sf::Event& e, const sf::Time& deltaTime = sf::Time(sf::seconds(0)), const sf::Vector2f& mousePos = sf::Vector2f(0, 0)) = 0;
 		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
 	};
 
@@ -65,7 +65,7 @@ namespace SFGF {
 
 	class GameObject : public sf::Drawable {
 	public:
-		virtual void Update(const sf::Event& e, const sf::Time& deltaTime, const sf::Vector2f& mousePos) = 0;
+		virtual void Update(sf::Event& e, const sf::Time& deltaTime, const sf::Vector2f& mousePos) = 0;
 		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
 	};
 
