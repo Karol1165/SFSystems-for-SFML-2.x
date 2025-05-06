a29c50a1cfc510b67435774c2593f93371ea18dd
Karol1165 <159713913+Karol1165@users.noreply.github.com>
2025-05-06 12:21:05 +0200

    Update framework.h

--- SFSystems/SFS-Utilties/framework.h
+++ SFSystems/SFS-Utilties/framework.h
@@ -1,9 +1,12 @@
 ﻿#pragma once
 
 #define WIN32_LEAN_AND_MEAN      
-
 #define NOMINMAX
 #include <windows.h>
+
+#ifdef _DEBUG
+#include<iostream>
+#endif
 
 #ifdef SFSUTILTIES_EXPORTS
 #define SFS_U_API __declspec(dllexport)
